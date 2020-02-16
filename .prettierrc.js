5a18f84d498e7e7c0495b80e2265fb2b728cd53b
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-02-17 00:21:43 +0200

    project init

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// .prettierrc.js
+module.exports = {
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  bracketSpacing: false
+};
---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+import {expect} from 'chai'
+
+describe('lorem ipsum test suite', () => {
+  it('dolor sit amet test', () => {
+    expect(true).to.be.true;
+  });
+});
