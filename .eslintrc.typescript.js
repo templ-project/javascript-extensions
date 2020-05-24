0e60608e2adbbd5e0b41325610e9e1f8d529f91f
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-05-24 21:57:09 +0300

    +

--- .eslintrc.typescript.js
+++ .eslintrc.typescript.js
@@ -1,18 +1,8 @@
 // .eslintrc.js
+
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended'],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  rules: {},
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
 };
