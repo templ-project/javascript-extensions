1b0f62d826b4b8925327014641098f8fab17a8db
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-05-25 00:03:50 +0300

    +

--- .scripts/change-language.js
+++ .scripts/change-language.js
@@ -119,7 +119,7 @@ const config = async () => {
         await unlink('.eslintrc.javascript.js');
         await unlink('.mocharc.javascript.js');
         await unlink('.prettierrc.javascript.js');
-        await unlink(path.join('test', 'intex.test.ts'));
+        await unlink(path.join('test', 'index.test.ts'));
         await unlink('tsconfig.json');
         await unlink('test/tsconfig.json');
         delete package.scripts['change:language'];
@@ -140,7 +140,7 @@ const config = async () => {
         await unlink('.eslintrc.typescript.js');
         await unlink('.mocharc.typescript.js');
         await unlink('.prettierrc.typescript.js');
-        await unlink(path.join('test', 'intex.test.js'));
+        await unlink(path.join('test', 'index.test.js'));
         delete package.scripts['change:language'];
       }
       break;
