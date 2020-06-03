40f8f083547fd08cecfb7cf8aedcfbee2822ba93
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-06-04 00:02:59 +0300

    coverage

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,7 @@ import {expect} from 'chai';
 import {hello} from '../src';
 
 describe('hello', function () {
-    it('hello("World") to return "Hello World!"', function () {
-        expect(hello('World')).to.equal('Hello World!');
-    });
+  it('hello("World") to return "Hello World!"', function () {
+    expect(hello('World')).to.equal('Hello World!');
+  });
 });
