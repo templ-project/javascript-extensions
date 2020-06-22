f67412d4d1f07febb06d1e02aaf08991d9802d81
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-06-22 22:17:47 +0300

    fixing readme

--- .scripts/change-language.js
+++ .scripts/change-language.js
@@ -49,7 +49,7 @@ const scripts = {
       "documentation build src/** -f html -o docs; documentation build src/** -f json -o docs.json",
     // esdocs: "esdoc; documentation build src/** -f json -o docs.json",
     "git-hook:pre-commit":
-      "npm run prettier:write && npm run lint:write && npm run jscpd && npm run test",
+      "npm run prettier:write && npm run lint:write && npm run jscpd && npm run depcruise && npm run test",
     prettier: "prettier ./{src,test}/**/*.js",
     // prettier: 'prettier ./{src,test}/**/*.{js,jsx}',
     jscpd: "jscpd ./src --blame --format javascript",
@@ -63,7 +63,7 @@ const scripts = {
     docs:
       "npx typedoc --out docs --json docs.json --readme none --theme minimal --mode file src",
     "git-hook:pre-commit":
-      "npm run prettier:write && npm run lint:write && npm run jscpd && npm run test",
+      "npm run prettier:write && npm run lint:write && npm run jscpd && npm run depcruise && npm run test",
     prettier: "prettier ./{src,test}/**/*.ts",
     // prettier: 'prettier ./{src,test}/**/*.{ts,tsx}',
     jscpd: "jscpd ./src --blame --format typescript",
@@ -208,6 +208,10 @@ const config = async () => {
 
     delete package.scripts["change:language"];
   }
+
+  await unlink('README.md');
+  await copyFile("README_TEMPLATE.md", "README.md");
+  await unlink('README_TEMPLATE.md');
 
   writeFile("package.json", JSON.stringify(package, null, 2), "utf-8");
 };
