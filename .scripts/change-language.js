34bc1e447cd765644bd94ec2d7c45cd615b42ab7
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-10-09 01:47:32 +0300

    +

--- .scripts/change-language.js
+++ .scripts/change-language.js
@@ -1,21 +1,24 @@
-const { prompt } = require('enquirer');
-const fs = require('fs');
+const { prompt } = require("enquirer");
+const fs = require("fs");
 
-const package = JSON.parse(fs.readFileSync('./package.json').toString());
+const package = JSON.parse(fs.readFileSync("./package.json").toString());
 
 /****************************************************************************
  * Methods
  ****************************************************************************/
 
-const to_rc = (obj, label = '.prettierrc') => {
-  fs.writeFileSync(`${label}.js`, `// ${label}.js
-module.exports = ${JSON.stringify(obj, null, 2)};`);
-}
+const to_rc = (obj, label = ".prettierrc") => {
+  fs.writeFileSync(
+    `${label}.js`,
+    `// ${label}.js
+module.exports = ${JSON.stringify(obj, null, 2)};`
+  );
+};
 
-const to_package = (obj, label = 'prettier') => {
+const to_package = (obj, label = "prettier") => {
   package[label] = obj;
   return package;
-}
+};
 
 const sortByKeys = (obj) => {
   let keys = Object.getOwnPropertyNames(obj).sort();
@@ -41,45 +44,49 @@ const removeKeys = (obj, keys) => {
   return newObj;
 };
 
-const mocharc = (alter = (tpl) => { tpl.require.push('@babel/register'); return tpl; }) => {
+const mocharc = (
+  alter = (tpl) => {
+    tpl.require.push("@babel/register");
+    return tpl;
+  }
+) => {
   const template = {
     recursive: true,
-    reporter: 'spec',
+    reporter: "spec",
     timeout: 5000,
     require: [
-      'chai/register-assert',  // Using Assert style
-      'chai/register-expect',  // Using Expect style
-      'chai/register-should',  // Using Should style
-    ]
+      "chai/register-assert", // Using Assert style
+      "chai/register-expect", // Using Expect style
+      "chai/register-should", // Using Should style
+    ],
   };
-  return alter ? alter(template) : template
-}
+  return alter ? alter(template) : template;
+};
 
 const jestrc = (alter = (tpl) => tpl) => {
   const template = {
-    "moduleFileExtensions": [
-      "js",
-      "json",
-      "ts"
-    ],
-    "rootDir": "src",
-    "testRegex": ".spec.ts$",
-    "transform": {
-      "^.+\\.(t|j)s$": "ts-jest"
+    moduleFileExtensions: ["js", "json", "ts"],
+    rootDir: "src",
+    testRegex: ".spec.ts$",
+    transform: {
+      "^.+\\.(t|j)s$": "ts-jest",
     },
-    "coverageDirectory": "../coverage",
-    "testEnvironment": "node"
+    coverageDirectory: "../coverage",
+    testEnvironment: "node",
   };
-  return alter ? alter(template) : template
-}
+  return alter ? alter(template) : template;
+};
 
-const eslintrc = (alter = (tpl) => Object.assign({}, tpl, {
-  parserOptions: {
-    parser: 'babel-eslint',
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-})) => {
+const eslintrc = (
+  alter = (tpl) =>
+    Object.assign({}, tpl, {
+      parserOptions: {
+        parser: "babel-eslint",
+        ecmaVersion: 2018,
+        sourceType: "module",
+      },
+    })
+) => {
   const template = {
     env: {
       browser: true,
@@ -87,19 +94,19 @@ const eslintrc = (alter = (tpl) => Object.assign({}, tpl, {
       node: true,
       mocha: true,
     },
-    extends: ['plugin:mocha/recommended', 'eslint:recommended'],
-    plugins: ['mocha'],
+    extends: ["plugin:mocha/recommended", "eslint:recommended"],
+    plugins: ["mocha"],
     root: true,
     rules: {
-      'consistent-return': 2,
+      "consistent-return": 2,
       indent: [1, 2],
-      'no-else-return': 1,
-      semi: [1, 'always'],
-      'space-unary-ops': 2,
+      "no-else-return": 1,
+      semi: [1, "always"],
+      "space-unary-ops": 2,
     },
   };
   return alter ? alter(template) : template;
-}
+};
 
 const prettierrc = (alter = (tpl) => tpl) => {
   const template = {
@@ -112,57 +119,79 @@ const prettierrc = (alter = (tpl) => tpl) => {
     bracketSpacing: false,
   };
   return alter ? alter(template) : template;
-}
+};
 /****************************************************************************
  * Settings
  ****************************************************************************/
 
-const question = [{
-  type: 'select',
-  name: 'language',
-  message: 'Choose JavaScript Flavor',
-  choices: ['javascript', 'typescript', {name: 'flow', disabled: true}, {name: 'coffee', disabled: true}],
-}, {
-  type: 'select',
-  name: 'testing',
-  message: 'Choose Src Folder',
-  choices: ['app', 'src'],
-  initial: 'src'
-}, {
-  type: 'select',
-  name: 'testing',
-  message: 'Choose Dist Folder',
-  choices: ['dist', 'lib'],
-  initial: 'dist'
-}, {
-  type: 'select',
-  name: 'testing',
-  message: 'Choose Testing Framework',
-  choices: [{name: 'jasmine', disabled: true}, 'jest', 'mocha'],
-  initial: 'mocha'
-}, {
-  type: 'multiselect',
-  name: 'inspectors',
-  message: 'Choose Code Inspectors',
-  choices: ['jscpd', 'dependency-cruiser'],
-  initial: ['jscpd']
-}, {
-  type: 'select',
-  name: 'templates',
-  message: 'Choose Git Repository Manager',
-  choices: [{name: 'bitbucket', disabled: true}, {name: 'gitea', disabled: true}, {name: 'gitee', disabled: true}, 'github', 'gitlab' ],
-  initial: ['github']
-}, {
-  type: 'select',
-  name: 'to',
-  message: 'Write configs to',
-  choices: [{name: 'rc', message: 'Separate Files'}, {name: 'package'} ],
-  initial: ['rc']
-}];
+const question = [
+  {
+    type: "select",
+    name: "language",
+    message: "Choose JavaScript Flavor",
+    choices: [
+      "javascript",
+      "typescript",
+      { name: "flow", disabled: true },
+      { name: "coffee", disabled: true },
+    ],
+  },
+  {
+    type: "select",
+    name: "testing",
+    message: "Choose Src Folder",
+    choices: ["app", "src"],
+    initial: "src",
+  },
+  {
+    type: "select",
+    name: "testing",
+    message: "Choose Dist Folder",
+    choices: ["dist", "lib"],
+    initial: "dist",
+  },
+  {
+    type: "select",
+    name: "testing",
+    message: "Choose Testing Framework",
+    choices: [{ name: "jasmine", disabled: true }, "jest", "mocha"],
+    initial: "mocha",
+  },
+  {
+    type: "multiselect",
+    name: "inspectors",
+    message: "Choose Code Inspectors",
+    choices: ["jscpd", "dependency-cruiser"],
+    initial: ["jscpd"],
+  },
+  {
+    type: "select",
+    name: "repository",
+    message: "Choose Git Repository Manager",
+    choices: [
+      { name: "bitbucket", disabled: true },
+      { name: "gitea", disabled: true },
+      { name: "gitee", disabled: true },
+      "github",
+      "gitlab",
+    ],
+    initial: ["github"],
+  },
+  {
+    type: "select",
+    name: "to",
+    message: "Write configs to",
+    choices: [{ name: "rc", message: "Separate Files" }, { name: "package" }],
+    initial: ["rc"],
+  },
+];
 
 const configs = {
   coffee: {
-    mocharc: mocharc((tpl) => { tpl.require.push('coffee-script/register'); return tpl; }),
+    mocharc: mocharc((tpl) => {
+      tpl.require.push("coffee-script/register");
+      return tpl;
+    }),
   },
   flow: {
     mocharc: mocharc(),
@@ -185,19 +214,19 @@ const configs = {
     mocharc: mocharc(),
     prettierrc: prettierrc(),
     scripts: {
-      docs: "documentation build src/** -f html -o docs; documentation build src/** -f json -o docs.json",
+      docs:
+        "documentation build src/** -f html -o docs; documentation build src/** -f json -o docs.json",
       prettier: "prettier ./{src,test}/**/*.{js,jsx}",
       jscpd: "jscpd ./src --blame --format javascript",
-      lint: 'eslint ./{src,test}/**/*.{js,jsx}',
+      lint: "eslint ./{src,test}/**/*.{js,jsx}",
       test: "npm run test:single -- './test/**/*.test.js'",
-      "test:single": "nyc --reporter=html --reporter=lcov --reporter=text --extension .js mocha --forbid-only",
+      "test:single":
+        "nyc --reporter=html --reporter=lcov --reporter=text --extension .js mocha --forbid-only",
     },
   },
   typescript: {
     dependencies: {},
     devDependencies: {
-      "@types/chai": "^4.2.11",
-      "@types/mocha": "^7.0.2",
       "@types/node": "^13.13.4",
       "@typescript-eslint/eslint-plugin": "^2.30.0",
       "@typescript-eslint/parser": "^2.30.0",
@@ -208,24 +237,32 @@ const configs = {
       typescript: "^3.8.3",
     },
     eslintrc: eslintrc((tpl) => {
-      tpl.extends.push('plugin:@typescript-eslint/recommended');
-      tpl.parser = '@typescript-eslint/parser';
-      tpl.plugins.push('@typescript-eslint');
+      tpl.extends.push("plugin:@typescript-eslint/recommended");
+      tpl.parser = "@typescript-eslint/parser";
+      tpl.plugins.push("@typescript-eslint");
       return tpl;
     }),
     jestrc: jestrc(),
-    mocharc: mocharc((tpl) => { tpl.require.push('ts-node/register'); return tpl; }),
-    prettierrc: prettierrc((tpl) => { tpl.parser = 'typescript'; return tpl; }),
+    mocharc: mocharc((tpl) => {
+      tpl.require.push("ts-node/register");
+      return tpl;
+    }),
+    prettierrc: prettierrc((tpl) => {
+      tpl.parser = "typescript";
+      return tpl;
+    }),
     scripts: {
-      docs: "npx typedoc --out docs --json docs.json --readme none --theme minimal --mode file src",
-      prettier: 'prettier ./{src,test}/**/*.{ts,tsx}',
+      docs:
+        "npx typedoc --out docs --json docs.json --readme none --theme minimal --mode file src",
+      prettier: "prettier ./{src,test}/**/*.{ts,tsx}",
       jscpd: "jscpd ./src --blame --format typescript",
-      lint: 'eslint ./{src,test}/**/*.{ts,tsx}',
+      lint: "eslint ./{src,test}/**/*.{ts,tsx}",
       test: "npm run test:single -- 'test/**/*.test.ts'",
-      "test:single": "nyc --reporter=html --reporter=lcov --reporter=text --extension .ts mocha --forbid-only",
+      "test:single":
+        "nyc --reporter=html --reporter=lcov --reporter=text --extension .ts mocha --forbid-only",
     },
-  }
-}
+  },
+};
 
 const init = (answers) => {
   // let writer = null;
@@ -237,14 +274,41 @@ const init = (answers) => {
   // (answers.to === 'rc') ? to_rc(configs[answers.language].prettierrc, '.prettierrc') : to_package(configs[answers.language].prettierrc, 'prettier');
 
   switch (answers.testing) {
-    case 'jasmine':
-    case 'jest':
+    case "jasmine":
+    case "jest":
       // .mocharc
-      (answers.to === 'rc') ? to_rc(configs[answers.language].jestrc, 'jest.config') : to_package(configs[answers.language].jestrc, 'jest');
+      answers.to === "rc"
+        ? to_rc(configs[answers.language].jestrc, "jest.config")
+        : to_package(configs[answers.language].jestrc, "jest");
+      package.devDependencies = Object.assign({}, package.devDependencies, {
+        "eslint-plugin-jest": "^23.18.0",
+        jest: "^26.1.0",
+        "ts-jest": "^26.1.1",
+        "ts-loader": "^8.0.0",
+      });
+      if (answers.language === "typescript") {
+        package.devDependencies = Object.assign({}, package.devDependencies, {
+          "@types/jest": "^26.0.4",
+        });
+      }
       break;
-    default: // mocha
+    default:
+      // mocha
       // .mocharc
-      (answers.to === 'rc') ? to_rc(configs[answers.language].mocharc, '.mocharc') : to_package(configs[answers.language].mocharc, 'mocha');
+      answers.to === "rc"
+        ? to_rc(configs[answers.language].mocharc, ".mocharc")
+        : to_package(configs[answers.language].mocharc, "mocha");
+      package.devDependencies = Object.assign({}, package.devDependencies, {
+        "eslint-plugin-mocha": "^7.0.1",
+        mocha: "^8.0.1",
+        "mocha-junit-reporter": "^2.0.0",
+      });
+      if (answers.language === "typescript") {
+        package.devDependencies = Object.assign({}, package.devDependencies, {
+          "@types/chai": "^4.2.11",
+          "@types/mocha": "^7.0.2",
+        });
+      }
   }
 
   // .jscpd
@@ -256,15 +320,38 @@ const init = (answers) => {
   //   to_rc(configs[answers.language].jscpd, '.jscpd');
   // }
 
+  switch (answers.repository) {
+    case 'bitbucket':
+    case 'gitea':
+    case 'gitee':
+      break;
+    case 'gitlab':
+      rimraf.sync('.github');
+      break;
+    default: // github
+      rimraf.sync('.gitlab');
+  }
 
-  package.dependencies = Object.assign({}, package.dependencies, configs[answers.language].dependencies)
-  sortByKeys(package.dependencies)
-  package.devDependencies = Object.assign({}, package.devDependencies, configs[answers.language].devDependencies)
-  sortByKeys(package.devDependencies)
-  removeKeys(package.devDependencies, ['enquirer'])
+  package.dependencies = Object.assign(
+    {},
+    package.dependencies,
+    configs[answers.language].dependencies
+  );
+  sortByKeys(package.dependencies);
+  package.devDependencies = Object.assign(
+    {},
+    package.devDependencies,
+    configs[answers.language].devDependencies
+  );
+  sortByKeys(package.devDependencies);
+  removeKeys(package.devDependencies, ["enquirer"]);
 
-  package.scripts = Object.assign({}, package.scripts, configs[answers.language].scripts)
-  sortByKeys(package.scripts)
+  package.scripts = Object.assign(
+    {},
+    package.scripts,
+    configs[answers.language].scripts
+  );
+  sortByKeys(package.scripts);
 
   // if (answers.inspectors.includes('dependency-cruiser')) {
   //   console.clear();
@@ -277,11 +364,11 @@ const init = (answers) => {
   //   removeKeys(package.devDependencies, ['dependency-cruiser']);
   // }
 
-  console.log(package)
+  console.log(package);
   // fs.writeFileSync('package.json', JSON.stringify(package, null, 2));
 };
 
 console.clear();
 prompt(question)
-  .then(answers => init(answers))
+  .then((answers) => init(answers))
   .catch(console.error);
