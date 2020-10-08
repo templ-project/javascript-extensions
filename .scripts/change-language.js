9accc7754a129e842506b568805285b08a1e490a
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-10-09 02:11:54 +0300

    +

--- .scripts/change-language.js
+++ .scripts/change-language.js
@@ -120,252 +120,279 @@ const prettierrc = (alter = (tpl) => tpl) => {
   };
   return alter ? alter(template) : template;
 };
+
+
+const srcCode = (answers) => {
+  fs.mkdirSync(answers.src, { recursive: true })
+
+  let template = ''
+  let ext = 'js'
+  switch (answers.language) {
+    case 'coffee':
+      ext = 'coffee'
+    case 'typescript':
+      ext = 'ts'
+      template = 'export const hello = (name: string): string => `Hello ${name}!`;'
+    case 'flow':
+    default:
+      template = 'export const hello = (name) => `Hello ${name}!`;'
+  }
+  fs.writeFileSync(path.join(answers.src, `index.${ext}`), template);
+}
+
 /****************************************************************************
  * Settings
  ****************************************************************************/
 
-const question = [
-  {
-    type: "select",
-    name: "language",
-    message: "Choose JavaScript Flavor",
-    choices: [
-      "javascript",
-      "typescript",
-      { name: "flow", disabled: true },
-      { name: "coffee", disabled: true },
-    ],
-  },
-  {
-    type: "select",
-    name: "testing",
-    message: "Choose Src Folder",
-    choices: ["app", "src"],
-    initial: "src",
-  },
-  {
-    type: "select",
-    name: "testing",
-    message: "Choose Dist Folder",
-    choices: ["dist", "lib"],
-    initial: "dist",
-  },
-  {
-    type: "select",
-    name: "testing",
-    message: "Choose Testing Framework",
-    choices: [{ name: "jasmine", disabled: true }, "jest", "mocha"],
-    initial: "mocha",
-  },
-  {
-    type: "multiselect",
-    name: "inspectors",
-    message: "Choose Code Inspectors",
-    choices: ["jscpd", "dependency-cruiser"],
-    initial: ["jscpd"],
-  },
-  {
-    type: "select",
-    name: "repository",
-    message: "Choose Git Repository Manager",
-    choices: [
-      { name: "bitbucket", disabled: true },
-      { name: "gitea", disabled: true },
-      { name: "gitee", disabled: true },
-      "github",
-      "gitlab",
-    ],
-    initial: ["github"],
-  },
-  {
-    type: "select",
-    name: "to",
-    message: "Write configs to",
-    choices: [{ name: "rc", message: "Separate Files" }, { name: "package" }],
-    initial: ["rc"],
-  },
-];
+// const question = [
+//   {
+//     type: "select",
+//     name: "language",
+//     message: "Choose JavaScript Flavor",
+//     choices: [
+//       "javascript",
+//       "typescript",
+//       { name: "flow", disabled: true },
+//       { name: "coffee", disabled: true },
+//     ],
+//   },
+//   {
+//     type: "select",
+//     name: "src",
+//     message: "Choose Src Folder",
+//     choices: ["app", "src"],
+//     initial: "src",
+//   },
+//   {
+//     type: "select",
+//     name: "dist",
+//     message: "Choose Dist Folder",
+//     choices: ["dist", "lib"],
+//     initial: "dist",
+//   },
+//   {
+//     type: "select",
+//     name: "testing",
+//     message: "Choose Testing Framework",
+//     choices: [{ name: "jasmine", disabled: true }, "jest", "mocha"],
+//     initial: "mocha",
+//   },
+//   {
+//     type: "multiselect",
+//     name: "inspectors",
+//     message: "Choose Code Inspectors",
+//     choices: ["jscpd", "dependency-cruiser"],
+//     initial: ["jscpd"],
+//   },
+//   {
+//     type: "select",
+//     name: "repository",
+//     message: "Choose Git Repository Manager",
+//     choices: [
+//       { name: "bitbucket", disabled: true },
+//       { name: "gitea", disabled: true },
+//       { name: "gitee", disabled: true },
+//       "github",
+//       "gitlab",
+//     ],
+//     initial: ["github"],
+//   },
+//   {
+//     type: "select",
+//     name: "to",
+//     message: "Write configs to",
+//     choices: [{ name: "rc", message: "Separate Files" }, { name: "package" }],
+//     initial: ["rc"],
+//   },
+// ];
 
-const configs = {
-  coffee: {
-    mocharc: mocharc((tpl) => {
-      tpl.require.push("coffee-script/register");
-      return tpl;
-    }),
-  },
-  flow: {
-    mocharc: mocharc(),
-  },
-  javascript: {
-    dependencies: {},
-    // dependencyCruiser: depcruise(),
-    devDependencies: {
-      "@babel/cli": "^7.8.4",
-      "@babel/core": "^7.9.6",
-      "@babel/node": "^7.8.7",
-      "@babel/preset-env": "^7.9.6",
-      "@babel/register": "^7.9.0",
-      "@rollup/plugin-babel": "^5.0.2",
-      "babel-eslint": "^10.1.0",
-      documentation: "^13.0.0",
-    },
-    eslintrc: eslintrc(),
-    jestrc: jestrc(),
-    mocharc: mocharc(),
-    prettierrc: prettierrc(),
-    scripts: {
-      docs:
-        "documentation build src/** -f html -o docs; documentation build src/** -f json -o docs.json",
-      prettier: "prettier ./{src,test}/**/*.{js,jsx}",
-      jscpd: "jscpd ./src --blame --format javascript",
-      lint: "eslint ./{src,test}/**/*.{js,jsx}",
-      test: "npm run test:single -- './test/**/*.test.js'",
-      "test:single":
-        "nyc --reporter=html --reporter=lcov --reporter=text --extension .js mocha --forbid-only",
-    },
-  },
-  typescript: {
-    dependencies: {},
-    devDependencies: {
-      "@types/node": "^13.13.4",
-      "@typescript-eslint/eslint-plugin": "^2.30.0",
-      "@typescript-eslint/parser": "^2.30.0",
-      "rollup-plugin-dts": "^1.4.7",
-      "rollup-plugin-typescript2": "^0.27.1",
-      "ts-node": "^8.10.1",
-      typedoc: "^0.17.7",
-      typescript: "^3.8.3",
-    },
-    eslintrc: eslintrc((tpl) => {
-      tpl.extends.push("plugin:@typescript-eslint/recommended");
-      tpl.parser = "@typescript-eslint/parser";
-      tpl.plugins.push("@typescript-eslint");
-      return tpl;
-    }),
-    jestrc: jestrc(),
-    mocharc: mocharc((tpl) => {
-      tpl.require.push("ts-node/register");
-      return tpl;
-    }),
-    prettierrc: prettierrc((tpl) => {
-      tpl.parser = "typescript";
-      return tpl;
-    }),
-    scripts: {
-      docs:
-        "npx typedoc --out docs --json docs.json --readme none --theme minimal --mode file src",
-      prettier: "prettier ./{src,test}/**/*.{ts,tsx}",
-      jscpd: "jscpd ./src --blame --format typescript",
-      lint: "eslint ./{src,test}/**/*.{ts,tsx}",
-      test: "npm run test:single -- 'test/**/*.test.ts'",
-      "test:single":
-        "nyc --reporter=html --reporter=lcov --reporter=text --extension .ts mocha --forbid-only",
-    },
-  },
-};
+// const configs = {
+//   coffee: {
+//     mocharc: mocharc((tpl) => {
+//       tpl.require.push("coffee-script/register");
+//       return tpl;
+//     }),
+//   },
+//   flow: {
+//     mocharc: mocharc(),
+//   },
+//   javascript: {
+//     dependencies: {},
+//     // dependencyCruiser: depcruise(),
+//     devDependencies: {
+//       "@babel/cli": "^7.8.4",
+//       "@babel/core": "^7.9.6",
+//       "@babel/node": "^7.8.7",
+//       "@babel/preset-env": "^7.9.6",
+//       "@babel/register": "^7.9.0",
+//       "@rollup/plugin-babel": "^5.0.2",
+//       "babel-eslint": "^10.1.0",
+//       documentation: "^13.0.0",
+//     },
+//     ext: 'js',
+//     eslintrc: eslintrc(),
+//     index: `export const hello = (name) => \`Hello \${name}!\`;`,
+//     jestrc: jestrc(),
+//     mocharc: mocharc(),
+//     prettierrc: prettierrc(),
+//     scripts: {
+//       docs:
+//         "documentation build src/** -f html -o docs; documentation build src/** -f json -o docs.json",
+//       prettier: "prettier ./{src,test}/**/*.{js,jsx}",
+//       jscpd: "jscpd ./src --blame --format javascript",
+//       lint: "eslint ./{src,test}/**/*.{js,jsx}",
+//       test: "npm run test:single -- './test/**/*.test.js'",
+//       "test:single":
+//         "nyc --reporter=html --reporter=lcov --reporter=text --extension .js mocha --forbid-only",
+//     },
+//   },
+//   typescript: {
+//     dependencies: {},
+//     devDependencies: {
+//       "@types/node": "^13.13.4",
+//       "@typescript-eslint/eslint-plugin": "^2.30.0",
+//       "@typescript-eslint/parser": "^2.30.0",
+//       "rollup-plugin-dts": "^1.4.7",
+//       "rollup-plugin-typescript2": "^0.27.1",
+//       "ts-node": "^8.10.1",
+//       typedoc: "^0.17.7",
+//       typescript: "^3.8.3",
+//     },
+//     eslintrc: eslintrc((tpl) => {
+//       tpl.extends.push("plugin:@typescript-eslint/recommended");
+//       tpl.parser = "@typescript-eslint/parser";
+//       tpl.plugins.push("@typescript-eslint");
+//       return tpl;
+//     }),
+//     ext: 'ts',
+//     index: `export const hello = (name: string): string => \`Hello \${name}!\`;`,
+//     jestrc: jestrc(),
+//     mocharc: mocharc((tpl) => {
+//       tpl.require.push("ts-node/register");
+//       return tpl;
+//     }),
+//     prettierrc: prettierrc((tpl) => {
+//       tpl.parser = "typescript";
+//       return tpl;
+//     }),
+//     scripts: {
+//       docs:
+//         "npx typedoc --out docs --json docs.json --readme none --theme minimal --mode file src",
+//       prettier: "prettier ./{src,test}/**/*.{ts,tsx}",
+//       jscpd: "jscpd ./src --blame --format typescript",
+//       lint: "eslint ./{src,test}/**/*.{ts,tsx}",
+//       test: "npm run test:single -- 'test/**/*.test.ts'",
+//       "test:single":
+//         "nyc --reporter=html --reporter=lcov --reporter=text --extension .ts mocha --forbid-only",
+//     },
+//   },
+// };
 
 const init = (answers) => {
-  // let writer = null;
+  srcCode(answers);
 
-  // // .eslintrc
-  // (answers.to === 'rc') ? to_rc(configs[answers.language].eslintrc, '.eslintrc') : to_package(configs[answers.language].eslintrc, 'eslint');
 
-  // // .prettierrc
-  // (answers.to === 'rc') ? to_rc(configs[answers.language].prettierrc, '.prettierrc') : to_package(configs[answers.language].prettierrc, 'prettier');
+  // // let writer = null;
 
-  switch (answers.testing) {
-    case "jasmine":
-    case "jest":
-      // .mocharc
-      answers.to === "rc"
-        ? to_rc(configs[answers.language].jestrc, "jest.config")
-        : to_package(configs[answers.language].jestrc, "jest");
-      package.devDependencies = Object.assign({}, package.devDependencies, {
-        "eslint-plugin-jest": "^23.18.0",
-        jest: "^26.1.0",
-        "ts-jest": "^26.1.1",
-        "ts-loader": "^8.0.0",
-      });
-      if (answers.language === "typescript") {
-        package.devDependencies = Object.assign({}, package.devDependencies, {
-          "@types/jest": "^26.0.4",
-        });
-      }
-      break;
-    default:
-      // mocha
-      // .mocharc
-      answers.to === "rc"
-        ? to_rc(configs[answers.language].mocharc, ".mocharc")
-        : to_package(configs[answers.language].mocharc, "mocha");
-      package.devDependencies = Object.assign({}, package.devDependencies, {
-        "eslint-plugin-mocha": "^7.0.1",
-        mocha: "^8.0.1",
-        "mocha-junit-reporter": "^2.0.0",
-      });
-      if (answers.language === "typescript") {
-        package.devDependencies = Object.assign({}, package.devDependencies, {
-          "@types/chai": "^4.2.11",
-          "@types/mocha": "^7.0.2",
-        });
-      }
-  }
+  // // // .eslintrc
+  // // (answers.to === 'rc') ? to_rc(configs[answers.language].eslintrc, '.eslintrc') : to_package(configs[answers.language].eslintrc, 'eslint');
 
-  // .jscpd
-  // if (answers.inspectors.includes('jscpd')) {
-  //   package.devDependencies = Object.assign({}, package.devDependencies, {
-  //     "jscpd": "^2.0.16",
-  //     "jscpd-badge-reporter": "^1.1.3",
-  //   })
-  //   to_rc(configs[answers.language].jscpd, '.jscpd');
+  // // // .prettierrc
+  // // (answers.to === 'rc') ? to_rc(configs[answers.language].prettierrc, '.prettierrc') : to_package(configs[answers.language].prettierrc, 'prettier');
+
+  // switch (answers.testing) {
+  //   case "jasmine":
+  //   case "jest":
+  //     // .mocharc
+  //     answers.to === "rc"
+  //       ? to_rc(configs[answers.language].jestrc, "jest.config")
+  //       : to_package(configs[answers.language].jestrc, "jest");
+  //     package.devDependencies = Object.assign({}, package.devDependencies, {
+  //       "eslint-plugin-jest": "^23.18.0",
+  //       jest: "^26.1.0",
+  //       "ts-jest": "^26.1.1",
+  //       "ts-loader": "^8.0.0",
+  //     });
+  //     if (answers.language === "typescript") {
+  //       package.devDependencies = Object.assign({}, package.devDependencies, {
+  //         "@types/jest": "^26.0.4",
+  //       });
+  //     }
+  //     break;
+  //   default:
+  //     // mocha
+  //     // .mocharc
+  //     answers.to === "rc"
+  //       ? to_rc(configs[answers.language].mocharc, ".mocharc")
+  //       : to_package(configs[answers.language].mocharc, "mocha");
+  //     package.devDependencies = Object.assign({}, package.devDependencies, {
+  //       "eslint-plugin-mocha": "^7.0.1",
+  //       mocha: "^8.0.1",
+  //       "mocha-junit-reporter": "^2.0.0",
+  //     });
+  //     if (answers.language === "typescript") {
+  //       package.devDependencies = Object.assign({}, package.devDependencies, {
+  //         "@types/chai": "^4.2.11",
+  //         "@types/mocha": "^7.0.2",
+  //       });
+  //     }
   // }
 
-  switch (answers.repository) {
-    case 'bitbucket':
-    case 'gitea':
-    case 'gitee':
-      break;
-    case 'gitlab':
-      rimraf.sync('.github');
-      break;
-    default: // github
-      rimraf.sync('.gitlab');
-  }
+  // // .jscpd
+  // // if (answers.inspectors.includes('jscpd')) {
+  // //   package.devDependencies = Object.assign({}, package.devDependencies, {
+  // //     "jscpd": "^2.0.16",
+  // //     "jscpd-badge-reporter": "^1.1.3",
+  // //   })
+  // //   to_rc(configs[answers.language].jscpd, '.jscpd');
+  // // }
 
-  package.dependencies = Object.assign(
-    {},
-    package.dependencies,
-    configs[answers.language].dependencies
-  );
-  sortByKeys(package.dependencies);
-  package.devDependencies = Object.assign(
-    {},
-    package.devDependencies,
-    configs[answers.language].devDependencies
-  );
-  sortByKeys(package.devDependencies);
-  removeKeys(package.devDependencies, ["enquirer"]);
-
-  package.scripts = Object.assign(
-    {},
-    package.scripts,
-    configs[answers.language].scripts
-  );
-  sortByKeys(package.scripts);
-
-  // if (answers.inspectors.includes('dependency-cruiser')) {
-  //   console.clear();
-  //   console.log('Proceding to configuring `dependency-cruiser`');
-  //   const program = require("commander");
-  //   program.init = true;
-  //   const cli = require('dependency-cruiser/src/cli');
-  //   cli([], program);
-  // } else {
-  //   removeKeys(package.devDependencies, ['dependency-cruiser']);
+  // switch (answers.repository) {
+  //   case 'bitbucket':
+  //   case 'gitea':
+  //   case 'gitee':
+  //     break;
+  //   case 'gitlab':
+  //     rimraf.sync('.github');
+  //     break;
+  //   default: // github
+  //     rimraf.sync('.gitlab');
   // }
 
-  console.log(package);
-  // fs.writeFileSync('package.json', JSON.stringify(package, null, 2));
+  // package.dependencies = Object.assign(
+  //   {},
+  //   package.dependencies,
+  //   configs[answers.language].dependencies
+  // );
+  // sortByKeys(package.dependencies);
+  // package.devDependencies = Object.assign(
+  //   {},
+  //   package.devDependencies,
+  //   configs[answers.language].devDependencies
+  // );
+  // sortByKeys(package.devDependencies);
+  // removeKeys(package.devDependencies, ["enquirer"]);
+
+  // package.scripts = Object.assign(
+  //   {},
+  //   package.scripts,
+  //   configs[answers.language].scripts
+  // );
+  // sortByKeys(package.scripts);
+
+  // // if (answers.inspectors.includes('dependency-cruiser')) {
+  // //   console.clear();
+  // //   console.log('Proceding to configuring `dependency-cruiser`');
+  // //   const program = require("commander");
+  // //   program.init = true;
+  // //   const cli = require('dependency-cruiser/src/cli');
+  // //   cli([], program);
+  // // } else {
+  // //   removeKeys(package.devDependencies, ['dependency-cruiser']);
+  // // }
+
+  // console.log(package);
+  // // fs.writeFileSync('package.json', JSON.stringify(package, null, 2));
 };
 
 console.clear();
