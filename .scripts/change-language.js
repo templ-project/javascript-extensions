b0e4d29b77c3abd087ab6a18d95774db4f8f2de1
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-10-22 03:25:28 +0300

    feat: jscpd

--- .scripts/change-language.js
+++ .scripts/change-language.js
@@ -233,7 +233,8 @@ const jscpd = (answers) => {
     'jscpd-badge-reporter': '^1.1.3',
   });
   package.scripts = Object.assign({}, package.scripts, {
-    jscpd: `jscpd ./${answers.src} --blame --format ${answers.language !== LANG_FLOW ? answers.language === LANG_COFFEE ? 'cofeescript' : answers.language : LANG_JS}`,
+    jscpd: `jscpd ./${answers.src} --blame --format ${answers.language !== LANG_FLOW ?
+      answers.language === LANG_COFFEE ? 'coffeescript' : answers.language : LANG_JS}`,
     'jscpd:html': 'npm run jscpd -- --reporters html',
   });
 };
--- .scripts/travis-test.sh
+++ .scripts/travis-test.sh
@@ -56,30 +56,34 @@ node -e 'var prettier = require("./.prettierrc.js"); if (prettier.parser != "cof
 npm i
 npm run prettier
 npm run lint
+npm run jscpd
 # npm test
-# do_clean
+do_clean
 
-# do_test flow src dist mocha github rc eslint
-# node -e 'var prettier = require("./.prettierrc.js"); if (prettier.parser != "flow") process.exit(1);'
-# npm i
-# npm run prettier
-# npm run lint
-# # npm test
-# do_clean
+do_test flow src dist mocha github rc eslint
+node -e 'var prettier = require("./.prettierrc.js"); if (prettier.parser != "flow") process.exit(1);'
+npm i
+npm run prettier
+npm run lint
+npm run jscpd
+# npm test
+do_clean
 
-# do_test javascript src dist mocha github rc eslint
-# node -e 'var prettier = require("./.prettierrc.js"); if (prettier.parser != "babel") process.exit(1);'
-# npm i
-# npm run prettier
-# npm run lint
-# # npm test
-# do_clean
+do_test javascript src dist mocha github rc eslint
+node -e 'var prettier = require("./.prettierrc.js"); if (prettier.parser != "babel") process.exit(1);'
+npm i
+npm run prettier
+npm run lint
+npm run jscpd
+# npm test
+do_clean
 
-# do_test typescript src dist mocha github rc eslint
-# node -e 'var prettier = require("./.prettierrc.js"); if (prettier.parser != "typescript") process.exit(1);'
-# npm i
-# npm run prettier
-# npm run lint
-# # npm test
-# do_clean
+do_test typescript src dist mocha github rc eslint
+node -e 'var prettier = require("./.prettierrc.js"); if (prettier.parser != "typescript") process.exit(1);'
+npm i
+npm run prettier
+npm run lint
+npm run jscpd
+# npm test
+do_clean
 
