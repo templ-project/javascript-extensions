a84256d3c5af3cf361d3fc18adb1fea15624540d
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-02-07 22:56:54 +0200

    +

--- .scripts/cl/package.js
+++ .scripts/cl/package.js
@@ -42,28 +42,18 @@ const syncPagkage = async (package) => {
     });
 
     for (const dependency of Object.keys(dependencies)) {
-      // await npm.commands.install([dependency], (er) => {
-      //   if (er) {
-      //     console.error(er)
-      //     process.exit(1)
-      //   }
-      // })
-    }
-    // catch errors
-    npm.commands.install([dependency], (er, data) => {
-      if (er) {
-        console.error(er)
-        process.exit(1)
-      }
+      await npm.commands.install([dependency], (er, data) => {
+        if (er) {
+          console.error(er)
+          process.exit(1)
+        }
 
-      console.log(data)
-    });
+        console.log(data)
+      });
+    }
   });
 
 
 }
-
-
-
 
 module.exports = syncPagkage
