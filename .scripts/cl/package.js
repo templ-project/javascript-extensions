886220c3f2e688317279ca1274c8793e4bd088c2
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-02-07 22:33:24 +0200

    +

--- .scripts/cl/package.js
+++ .scripts/cl/package.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
+const npm = require('npm');
+
 const twig = require('./cl/twig');
 
 export const syncPagkage = (package) => {
-  package.dependencies = sortByKeys(package.dependencies || {});
-  package.devDependencies = sortByKeys(package.devDependencies || {});
-  package.peerDependencies = sortByKeys(package.peerDependencies || {});
+  const dependencies = sortByKeys(package.dependencies || {});
+  const devDependencies = sortByKeys(package.devDependencies || {});
+  const peerDependencies = sortByKeys(package.peerDependencies || {});
   package.scripts = sortByKeys(package.scripts || {});
 
   package.husky = {
@@ -22,7 +24,21 @@ export const syncPagkage = (package) => {
     await fs.remove("./package-lock.json");
   } catch (e) {}
   await fs.promises.writeFile("./package.json", rendered);
+
+
+  for (const dependency of  Object.keys(dependencies)) {
+    npm.commands.install([dependency], (er) => {
+      if (er) {
+        console.error(er)
+        process.exit(1)
+      }
+    })
+  }
+
+
 }
+
+
 
 
 module.exports = syncPagkage
