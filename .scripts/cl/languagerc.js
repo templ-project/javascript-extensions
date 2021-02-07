68dda51c5fc6c406e6a3725a880b7ef803dfb5ba
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-02-08 00:48:47 +0200

    +

--- .scripts/cl/languagerc.js
+++ .scripts/cl/languagerc.js
@@ -5,7 +5,7 @@ const languagerc = (answers, package) => {
   if (answers.language === LANG_COFFEE) {
     package.newDevDependencies = {
       ...(package.newDevDependencies || {}),
-      coffeescript: '?',
+      coffeescript: '^2.5.1',
     };
   } else if (answers.language === LANG_TS) {
     package.newDevDependencies = {
