01975d4179471e64393a7c50b0952d0d60d48895
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-02-11 00:14:55 +0200

    importSort.*.style

--- .scripts/cl/prettierrc.js
+++ .scripts/cl/prettierrc.js
@@ -50,8 +50,8 @@ const prettierrc = async (answers, package) => {
       ...(package.importSort || {}),
       '.js, .jsx, .ts, .tsx': {
         parser: answers.language === LANGS.LANG_TS ? 'typescript' : 'babylon',
-        // style: 'eslint',
-        style: 'module',
+        style: 'eslint',
+        // style: 'module',
       }
     }
   }
